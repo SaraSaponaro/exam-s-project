0b3a1b15936d55cdf4fcbe9dc2ece207879f94a5
SaraSaponaro <sarasaponaro@MBP-di-Sara-2.homenet.telecomitalia.it>
2019-12-22 14:20:43 +0100

    initial import

--- max_var_point_s.py
+++ max_var_point_s.py
@@ -3,14 +3,14 @@ il bordo è dato dai punti dei raggi con la massimo std'''
 import pylab as plt
 import numpy as np
 from scipy.ndimage.filters import generic_filter
+from skimage.filters import threshold_otsu
 
 '''funsione che binarizza img'''
-def imbinarize(img, threshold):
+def imbinarize(img):
+    thresh = threshold_otsu(img)
     img[img >= threshold] = 1
     img[img < threshold] = 0
     return img
-
-
 
 
 def max_var_point(normalized, ROI, Ray_masks, NL, nhood):
@@ -19,19 +19,20 @@ def max_var_point(normalized, ROI, Ray_masks, NL, nhood):
     #plt. figure()
     #plt.imshow(J, cmpa='gray')
 
-    B_pints=[]
-    shape=np.shaoe(normalized)
+    B_points=[]
+    shape=np.shape(normalized)
     roughborder=np.zeros(shape)
 
     for it in range(1, NL):
-        Jmasked=J*Ray_masks[:,:,it]     #itero sulla terza dimensione
-        '''riguarda come è fatta ROI e decidi come impostare la soglia'''
-        Jmasked=Jmasked*imbinarize(ROI, threshold)
-
-        [c1, c2]= np.where(Jmasked==np.max(Jmasked[:]);
+        #itero sulla terza dimensione: Ray_masks immagini 162x162, 32 (raggi)
+        Jmasked=J*Ray_masks[:,:,it]     #J*raggi=maschera dell'img
+        threshold = np.max()
+        Jmasked=Jmasked*imbinarize(ROI)
+        [c1, c2]= np.where(Jmasked==np.max(Jmasked))    #mi devo accertare che prenda gli indici giusti
         '''devo riguardare su matlab
         B_points=[B_points; c1(1), c2(1) ,J(c1(1),c2(1))]
         '''
-        roughborder[c1(1),c2(2)]=normalized[c1(1),c2(1)]
+        roughborder[c1(1),c2(2)]=normalized[c1(1),c2(1)]  #copio pixel img all'interno della matrice
+
 
     #come posso chiudere i bordi ?!
