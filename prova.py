3993ded636c5c4016d5f9d9d42a8ae0d82f63e49
Luigi Masturzo <luigimasturzo@MBP-di-Luigi.inol.it>
2019-12-25 11:35:24 +0100

    parte grezza di  trova il bordo

--- prova.py
+++ prova.py
@@ -29,9 +29,11 @@ im_conv=convolve2d(image, k )
 im_resized = resize(im_conv, (126,126), preserve_range=True)
 im_norm = im_resized/np.max(im_resized)
 
-plt.figure()
+plt.figure('immagine normalizzata')
 plt.imshow(im_norm)
-plt.show()
+plt.figure('immagine')
+plt.imshow(image)
+#plt.show()
 
 #%%SEGMENTATION
 "per il momento seleziono una roi a mano dell'immagine"
@@ -91,11 +93,11 @@ for _ in range(0,NL):
         Ray_mask[i,j]=1 
     Ray_masks.append(Ray_mask)
     
-plt.figure()
+plt.figure('raggio casuale')
 plt.imshow(Ray_masks[10])
 plt.imshow(im_norm, alpha=0.5)
 plt.plot(center[0],center[1], 'r.')
-plt.show()
+#plt.show()
 
 #%% max variance points
 '''funsione che binarizza img'''
@@ -126,16 +128,80 @@ for _ in range (0, NL):
     d.append(Jmasked[w[0][0],w[1][0]])
     roughborder[p_x, p_y]=im_norm[w[0][0], w[1][0]]
 
-plt.figure()
+plt.figure('bordo puntallato')
 plt.imshow(roughborder)
-plt.imshow(im_norm, alpha=0.5)
+plt.imshow(im_norm, alpha=0.3)
+plt.plot(p_x,p_y)   #dovrebbe vwnire piu o meno cosi
+#plt.show()
 
-#%%
-from scipy.interpolate import Rbf
-rbfi=Rbf(p_x,p_y,d)
-xi = yi = np.linspace(np.min(p_x), np.max(p_x),33)
-di = rbfi(xi, yi)
-roughborder[int(x1),int( yi)]=1
+
+#%% cerchiamo di unire i pixel per fare il bordo
+'mi serve dopo-> scrivo una fuzione per trovare la distanza euclidea'
+def distanza(x1,y1,x2,y2):
+    distanza_euclidea=np.sqrt((x1-x2)**2 + (y1-y2)**2)
+    return distanza_euclidea
+
+
+'questa liste mi rappresentano le coordinate x e y dei punti che mi formeranno il bordo'
+bordo_x=[]
+bordo_y=[]
+
+
+'inizio il tutto partendo da un punto casuale del bordo: alla fine dovrà coincidere con ultimo per poter chiudere il bordo'
+
+a=0
+b=0
+plt.figure()
+for _ in range(0,1):
+    start = im_norm[p_x[_], p_y[_]]         #pixel da cui parto
+    stop = im_norm[p_x[_+1], p_y[_+1]]      #pixel in cui finisco
+
+    'pur essendo poco ottimizzato, io per ogni pixel ho 8 vicini, salvo le loro coordinate in una lista'
+
+    N1=im_norm[p_x[_]-1, p_y[_]]
+    N2=im_norm[p_x[_]-1, p_y[_]+1]
+    N3=im_norm[p_x[_], p_y[_]+1]
+    N4=im_norm[p_x[_]+1, p_y[_]+1]
+    N5=im_norm[p_x[_]+1, p_y[_]]
+    N6=im_norm[p_x[_]+1, p_y[_]-1]
+    N7=im_norm[p_x[_]+1, p_y[_]-1]
+    N8=im_norm[p_x[_]-1, p_y[_]-1]
+
+    vicino_x=[p_x[_]-1,p_x[_]-1,p_x[_],p_x[_]+1,p_x[_]+1,p_x[_]+1,p_x[_]+1,p_x[_]-1]
+    vicino_y=[p_y[_],p_y[_]+1,p_y[_]+1,p_y[_]+1,p_y[_],p_y[_]-1,p_y[_]-1,p_y[_]-1]
+
+    '''plt.scatter(p_x[_], p_y[_],label='start')
+
+    plt.scatter(p_x[_]-1, p_y[_],label='vicino')
+    plt.scatter(p_x[_]-1, p_y[_]+1,label='vicino')
+    plt.scatter(p_x[_], p_y[_]+1,label='vicino')
+    plt.scatter(p_x[_]+1, p_y[_]+1,label='vicino')
+    plt.scatter(p_x[_]+1, p_y[_],label='vicino')
+    plt.scatter(p_x[_]+1, p_y[_]-1,label='vicino')
+    plt.scatter(p_x[_]+1, p_y[_]-1,label='vicino')
+    plt.scatter(p_x[_]-1, p_y[_]-1,label='vicino')'''
+
     
 
-griddata(points, values, (grid_x, grid_y), method='linear')
+    'ora devo caloclare la distanza euclidde tra il vicino e stop'
+    distanza_list=[]
+    c=0
+    for __ in range(0,len(vicino_x)):
+        c=distanza(vicino_x[__],vicino_y[__],p_x[_+1], p_y[_+1])
+        distanza_list.append(c)
+
+    distanza_list=np.asarray(distanza_list)
+    d=np.where(distanza_list==distanza_list.min())
+
+    bordo_x.append(vicino_x[int(d[0])])
+    bordo_y.append(vicino_y[int(d[0])])
+    
+  
+    
+
+
+#plt.legend()
+#plt.show()
+
+
+
