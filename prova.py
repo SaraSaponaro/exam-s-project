fb299cdf8e46e7d792a412d0762ac91897e75e41
SaraSaponaro <sarasaponaro@MBP-di-Sara-2.homenet.telecomitalia.it>
2019-12-27 11:16:55 +0100

    package

--- prova.py
+++ prova.py
@@ -5,9 +5,8 @@ import imageio
 from scipy.signal import convolve2d
 from skimage.transform import resize
 from scipy.ndimage.filters import generic_filter
-from skimage.filters import threshold_otsu
 from draw_radial_line import draw_radial_lines
-
+from define_border import define_border
 
 "leggo il file"
 fileID='0016p1_2_1.png'
@@ -84,115 +83,11 @@ plt.plot(center[0],center[1], 'r.')
 plt.show()
 
 #%% max variance points
-'''funsione che binarizza img'''
-def imbinarize(img):
-    thresh = threshold_otsu(img)
-    img[img >= thresh] = 1
-    img[img < thresh] = 0
-    return img
 
-J = generic_filter(im_norm, np.std, size=size_nhood_variance)
-'''
-plt. figure()
-plt.imshow(J, cmap='gray')
-ptl.show()
-'''
-
-B_points=[]
-roughborder=np.zeros(np.shape(im_norm))
-p_x=[]
-p_y=[]
-d=[]
-for _ in range (0, NL):
-    Jmasked=J*Ray_masks[_]     #J*raggi=maschera dell'img
-    Jmasked=Jmasked*imbinarize(ROI)
-    w = np.where(Jmasked==np.max(Jmasked))
-    p_y.append(w[0][0])
-    p_x.append(w[1][0])
-    d.append(Jmasked[w[0][0],w[1][0]])
-    #roughborder[p_x, p_y]=im_norm[w[0][0], w[1][0]]
-
-#%% cerchiamo di unire i pixel per fare il bordo
-'mi serve dopo-> scrivo una fuzione per trovare la distanza euclidea'
-def distanza(x1,y1,x2,y2):
-    distanza_euclidea=np.sqrt((x1-x2)**2 + (y1-y2)**2)
-    return distanza_euclidea
-
-'devo definire una funzione con il while che mi faccia il loop (ovvero che mi riempia i pxel) tra un punto del bordo e laltro'
-def find_border(_, p_x, p_y):
-
-    'liste vuote che riempirò con i pixel trovati'
-    bordo_x=[]
-    bordo_y=[]
-
-    distanza_finale=100
-
-    while (distanza_finale >= 1):     #finchè non raggiungo il pixel stop
-
-        'trovo i pixel vicini'
-        vicino_x=[p_x[_]-1,p_x[_]-1,p_x[_],p_x[_]+1,p_x[_]+1,p_x[_]+1,p_x[_]+1,p_x[_]-1]
-        vicino_y=[p_y[_],p_y[_]+1,p_y[_]+1,p_y[_]+1,p_y[_],p_y[_]-1,p_y[_]-1,p_y[_]-1]
-
-
-        'trovo la distanza e scelgo quella minima'
-        distanza_list=[]
-        c=0
-        for __ in range(0,len(vicino_x)):
-            c=distanza(vicino_x[__],vicino_y[__],p_x[_+1], p_y[_+1])
-            distanza_list.append(c)
-
-        distanza_list=np.asarray(distanza_list)
-        'scelgo il pixel a cui mi corrisponde la distanza minima rispetto a quello di stop'
-        d=np.argmin(distanza_list)                 #np.where(distanza_list==distanza_list.min())
-        distanza_finale=distanza_list[int(d)]
-        #print('d=',d)
-        #print('distanza0 ',distanza_finale)
-
-        'coordinate del pixel prescelto'
-        pixel_x=vicino_x[int(d)]
-        pixel_y=vicino_y[int(d)]
-
-        'mi salvo la posizione del pixel, perchè alla fine dovrò concatenare tutte queste liste ed otterrò il bordo'
-        bordo_x.append(pixel_x)
-        bordo_y.append(pixel_y)
-
-        #vediamo se va tutto bene
-        #print('x=',pixel_x)
-        #print('y=',pixel_y)
-
-
-        'il mio nuovo pixel da cui trovare tutti i vicini è quello prescelto'
-        p_x[_]=pixel_x
-        p_y[_]=pixel_y
-
-
-
-    return bordo_x, bordo_y
-
-
-'inizio il tutto partendo da un punto casuale del bordo: alla fine dovrà coincidere con ultimo per poter chiudere il bordo'
-bordofinale_x=[]
-bordofinale_y=[]
-
-for _ in range(0,NL-1):        #poi bisogna mettere for i in range(0,NL)
-    'definisco le liste vuote cosi ad ogni for me le svuota'
-    print(_)
-    bordoo_x=[]
-    bordoo_y=[]
-    #start = im_norm[p_x[_], p_y[_]]         #pixel da cui parto
-    #stop = im_norm[p_x[_+1], p_y[_+1]]      #pixel in cui finisco
-
-    #print('x_in = ',p_x[0])
-    #print('y_in = ',p_y[0])
-
-    bordoo_x, bordoo_y = find_border(_, p_x, p_y)
-    roughborder[bordoo_y, bordoo_x]=1
-    bordofinale_x += bordoo_x
-    bordofinale_y += bordoo_y
-
+roughborder,bordofinale_x,bordofinale_y=define_border(im_norm, NL, ROI,size_nhood_variance, Ray_masks)
 
 plt.figure()
-#plt.plot(bordofinale_x,bordofinale_y,'.')
+plt.plot(bordofinale_x,bordofinale_y,'.')
 plt.imshow(roughborder)
 plt.imshow(im_norm, alpha=0.5)
 plt.show()
