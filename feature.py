11d8164eef7df63c9852d6a54e4b18c0b35dcd7a
SaraSaponaro <sarasaponaro@MBP-di-Sara-2.homenet.telecomitalia.it>
2019-12-29 12:16:05 +0100

    feature

--- feature.py
+++ feature.py
@@ -6,21 +6,17 @@ from define_border import distanza
 
 
 #leggo il file
-file_id_mass='prova.png' #'result/0016p1_2_1_massonly.png'
 file_id_mask='result/0016p1_2_1_mask.png'
-
-
-mass_only=imageio.imread(file_id_mass)
 mask_only=imageio.imread(file_id_mask)
 
 #%%
-def mass_area(mass_only):
+def mass_area(mask_only):
     a=np.where(mass_only!=0)
     area= np.shape(a)[1]
     return area 
 
-def mass_perimetro(mass_only):
-    contours = measure.find_contours(mass_only, 1)
+def mass_perimetro(mask_only):
+    contours = measure.find_contours(mask_only, 0)
     return np.shape(contours)[1]
 
 "c=1 se cerchio unitest"
@@ -28,14 +24,26 @@ def circularity(area, perimetro):
     c = 4*np.pi*area/(perimetro**2)
     return c
     
-def mu_NRL(mass_only, center):
-    contours = measure.find_contours(mass_only, 1)
-    for n, contour in enumerate(contours):
-        x=int(contour[:, 1])
-        y=int(contour[:, 0])
-        d= distanza(center[0], center[1],x, y)
-'''prova 
-        
-contours = measure.find_contours(mass_only, 1)
-np.shape(contours)[1]
-'''  
+def mu_NRL(mask_only, center, perimetro):
+    contours = measure.find_contours(mask_only, 0)
+    arr=  contours[0]
+    arr = arr.flatten('F')
+    y = arr[0:99]
+    x = arr[99:]
+    d=distanza(center[0],center[1], x, y)
+    d_mean=np.sum(d)/perimetro
+    return d, d_mean 
+
+def sigma_NRL(d,d_mean, perimetro):
+    somm=np.sum((d-d_mean)**2)
+    return np.sqrt(somm/perimetro)
+    
+   
+    
+    
+    
+    
+    
+    
+    
+    
