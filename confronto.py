546982c00067c2344c872ca0195ff59afe0b184e
SaraSaponaro <sarasaponaro@MBP-di-Sara-2.homenet.telecomitalia.it>
2019-12-31 09:36:37 +0100

    imag_conf

--- confronto.py
+++ confronto.py
@@ -5,19 +5,18 @@ import pylab as plt
 
 #change extension
 '''
-confronto = glob.glob('ref/*_mask.pgm')
+confronto = glob.glob('ref/*.pgm')
+
 for i in range(0,len(confronto),1):
     source = confronto[i]
     filename, file_extension = os.path.splitext(source)
-    dest = 'ref/'+filename+'.png'
+    dest = filename+'.png'
     os.rename(source, dest) 
 '''
+mask = glob.glob('result/0016p1_2_1_mask.png')
+confronto = glob.glob('ref/0016p1_2_1_mass_mask.png')
 
-mask = glob.glob('result/*_mask.png')
+plt.imshow(Image.open(confronto[0]), cmap='gray')
+#plt.imshow(Image.open(mask[0]), alpha=0.5, cmap='gray')
+plt.imshow(Image.open('ref/0016p1_2_1_resized.png'), alpha=0.7)
 
-filename, file_extension = os.path.splitext(source)
-
-confronto = glob.glob('ref/*_mask.png')
-
-plt.imshow(Image.open(confronto[0]))
-#plt.imshow(mask[0])
