b5bb5529a78c02fde1e85afc812442858561d5fe
SaraSaponaro <sarasaponaro@MBP-di-Sara-2.homenet.telecomitalia.it>
2020-01-03 11:40:30 +0100

    img_small_sample

--- confronto.py
+++ confronto.py
@@ -5,18 +5,18 @@ import pylab as plt
 
 #change extension
 '''
-confronto = glob.glob('ref/*.pgm')
+confronto = glob.glob('img/*.pgm')
 
 for i in range(0,len(confronto),1):
     source = confronto[i]
     filename, file_extension = os.path.splitext(source)
     dest = filename+'.png'
-    os.rename(source, dest) 
+    os.rename(source, dest)
 '''
-mask = glob.glob('result/0016p1_2_1_mask.png')
-confronto = glob.glob('ref/0016p1_2_1_mass_mask.png')
+mask = glob.glob('result/0036p1_1_1_mask.png')
+confronto = glob.glob('ref/0036p1_1_1_mass_mask.png')
 
 plt.imshow(Image.open(confronto[0]), cmap='gray')
 plt.imshow(Image.open(mask[0]), alpha=0.5, cmap='gray')
 #plt.imshow(Image.open('ref/0036p1_1_1_resized.png'), alpha=0.7)
-
+plt.show()
--- segmentation_program.py
+++ segmentation_program.py
@@ -3,13 +3,13 @@ import numpy as np
 import imageio
 import os
 import logging
+import glob
 from scipy.signal import convolve2d
 from skimage.transform import  rescale
 from PIL import Image
-import PIL
 from scipy import ndimage
 from draw_radial_line import draw_radial_lines
-from define_border import define_border_new, distanza, imbinarize
+from define_border import define_border_new, distanza
 logging.basicConfig(level=logging.INFO)
 
 def process_img(image, smooth_factor, scale_factor):
@@ -41,113 +41,119 @@ def find_center(x_max, y_max):
 
 if __name__ == '__main__':
 
-    f = open('center_list.txt', 'a')
+
 
     logging.info('Si legge il file.')
-    fileID='0016p1_2_1.png'
+    #fileID='0016p1_2_1.png'
     #fileID='0025p1_4_1.png'
     #fileID='0036p1_1_1.png'
 
-    image=imageio.imread(fileID)
+    #read all files
+    fileID = glob.glob('img/*.png')
 
-    filename, file_extension = os.path.splitext(fileID)
-    path_out = 'result/'
+    for _ in range (2, len(fileID)):
+        f = open('center_list.txt', 'a')
+        image=imageio.imread(fileID[_])
 
-    if (os.path.exists(path_out)==False):
-        logging.info ('Creating folder for save results.\n')
-        os.makedirs('result')
+        filename, file_extension = os.path.splitext(fileID[_])
+        filename=os.path.basename(filename)
+        path_out = 'result/'
 
-    file_out=path_out+filename+'_resized'+file_extension
-    mask_out=path_out+filename+'_mask'+file_extension
+        if (os.path.exists(path_out)==False):
+            logging.info ('Creating folder for save results.\n')
+            os.makedirs('result')
 
-    logging.info('Defining parameters ')
-    smooth_factor= 8
-    scale_factor= 8
-    size_nhood_variance=5
-    NL=33
+        file_out=path_out+filename+'_resized'+file_extension
+        mask_out=path_out+filename+'_mask'+file_extension
 
-    logging.info('Processing image.')
-    im_norm, im_resized = process_img(image, smooth_factor, scale_factor)
+        logging.info('Defining parameters ')
+        smooth_factor= 8
+        scale_factor= 8
+        size_nhood_variance=5
+        NL=33
 
-    logging.info('Starting the image segmentation.')
-    logging.info('Enter ROIs coordinates that contains the mass.')
-    y1=int(input('Enter y1: '))
-    x1=int(input('Enter x1: '))
-    y2=int(input('Enter y2: '))
-    x2=int(input('Enter x2: '))
+        logging.info('Processing image {}'.format(filename))
+        im_norm, im_resized = process_img(image, smooth_factor, scale_factor)
 
-    ROI=np.zeros(np.shape(im_norm))
-    ROI[y1:y2,x1:x2]=im_norm[y1:y2,x1:x2]
-    y_max,x_max=np.where(ROI==np.max(ROI))
-    center = find_center(x_max, y_max)
+        logging.info('Starting the image segmentation.')
+        logging.info('Enter ROIs coordinates that contains the mass.')
+        y1=int(input('Enter y1: '))
+        x1=int(input('Enter x1: '))
+        y2=int(input('Enter y2: '))
+        x2=int(input('Enter x2: '))
 
-    f.write('{} \t {} \t {}\n'.format(fileID, center[0], center[1]))
+        ROI=np.zeros(np.shape(im_norm))
+        ROI[y1:y2,x1:x2]=im_norm[y1:y2,x1:x2]
+        y_max,x_max=np.where(ROI==np.max(ROI))
+        center = find_center(x_max, y_max)
 
-    logging.info('Showing ROI and center.' )
-    plt.figure()
-    plt.title('ROI')
-    plt.imshow(im_norm)
-    plt.imshow(ROI, alpha=0.3)
-    plt.plot(center[0], center[1], 'r.')
-    plt.colorbar()
-    plt.show()
+        f.write('{} \t {} \t {}\n'.format(filename, center[0], center[1]))
 
-    logging.info('Drawing radial lines.')
-    #define length of ray.
-    R=int(distanza(x1,y1,x2,y2)/2)
-    Ray_masks=draw_radial_lines(ROI,center,R,NL)
+        logging.info('Showing ROI and center.' )
+        plt.figure()
+        plt.title('ROI')
+        plt.imshow(im_norm)
+        plt.imshow(ROI, alpha=0.3)
+        plt.plot(center[0], center[1], 'r.')
+        plt.colorbar()
+        plt.show()
 
-    plt.figure()
-    plt.title('Some of rays found.')
-    plt.imshow(Ray_masks[0])
-    plt.imshow(Ray_masks[9])
-    plt.imshow(Ray_masks[20])
-    plt.imshow(Ray_masks[27])
-    plt.imshow(im_norm, alpha=0.3)
-    plt.imshow(ROI, alpha=0.5)
-    plt.plot(center[0],center[1], 'r.')
-    plt.colorbar()
-    plt.show()
+        logging.info('Drawing radial lines.')
+        #define length of ray.
+        R=int(distanza(x1,y1,x2,y2)/2)
+        Ray_masks=draw_radial_lines(ROI,center,R,NL)
 
-    logging.info('Finding teh border.')
-    roughborder,bordofinale_x,bordofinale_y=define_border_new(im_norm, NL, ROI,size_nhood_variance, Ray_masks)
-    fill=ndimage.binary_fill_holes(roughborder).astype(int)
+        plt.figure()
+        plt.title('Some of rays found.')
+        plt.imshow(Ray_masks[0])
+        plt.imshow(Ray_masks[9])
+        plt.imshow(Ray_masks[20])
+        plt.imshow(Ray_masks[27])
+        plt.imshow(im_norm, alpha=0.3)
+        plt.imshow(ROI, alpha=0.5)
+        plt.plot(center[0],center[1], 'r.')
+        plt.colorbar()
+        plt.show()
 
-    plt.figure()
-    plt.title('Mask of segmented mass.')
-    plt.imshow(fill)
-    plt.show()
+        logging.info('Finding teh border.')
+        roughborder,bordofinale_x,bordofinale_y=define_border_new(im_norm, NL, ROI,size_nhood_variance, Ray_masks)
+        fill=ndimage.binary_fill_holes(roughborder).astype(int)
 
-    logging.info('Refining segmentation results.')
-    for _ in range(0, len(bordofinale_x)):
-        center_raff = [bordofinale_x[_], bordofinale_y[_]]
-        Ray_masks_raff = draw_radial_lines(ROI,center_raff,int(R/5),NL)
-        roughborder_raff, _ , _ = define_border_new(im_norm, NL, ROI,size_nhood_variance, Ray_masks_raff)
-        roughborder+=roughborder_raff
+        plt.figure()
+        plt.title('Mask of segmented mass.')
+        plt.imshow(fill)
+        plt.show()
 
-    fill_raff=ndimage.binary_fill_holes(roughborder).astype(int)
+        logging.info('Refining segmentation results.')
+        for _ in range(0, len(bordofinale_x)):
+            center_raff = [bordofinale_x[_], bordofinale_y[_]]
+            Ray_masks_raff = draw_radial_lines(ROI,center_raff,int(R/5),NL)
+            roughborder_raff, _ , _ = define_border_new(im_norm, NL, ROI,size_nhood_variance, Ray_masks_raff)
+            roughborder+=roughborder_raff
 
-    plt.figure()
-    plt.title('Final mask of segmented mass.')
-    plt.imshow(fill_raff)
-    plt.show()
+        fill_raff=ndimage.binary_fill_holes(roughborder).astype(int)
 
-    logging.info('Showing result')
-    mass_only = fill_raff*im_norm
+        plt.figure()
+        plt.title('Final mask of segmented mass.')
+        plt.imshow(fill_raff)
+        plt.show()
 
-    plt.figure()
-    plt.title('Segmented mass.')
-    plt.imshow(mass_only)
-    plt.colorbar()
-    plt.show()
+        logging.info('Showing result')
+        mass_only = fill_raff*im_norm
 
-    logging.info('Savening result.')
-    im = im_resized.astype(np.uint8)
-    im = Image.fromarray(im, mode='P')
-    im.save(file_out)
+        plt.figure()
+        plt.title('Segmented mass.')
+        plt.imshow(mass_only)
+        plt.colorbar()
+        plt.show()
 
-    fill_raff = fill_raff.astype(np.int8)
-    im1 = Image.fromarray(fill_raff, mode='L')
-    im1.save(mask_out)
+        logging.info('Savening result.')
+        im = im_resized.astype(np.uint8)
+        im = Image.fromarray(im, mode='P')
+        im.save(file_out)
 
-    f.close()
+        fill_raff = fill_raff.astype(np.int8)
+        im1 = Image.fromarray(fill_raff, mode='L')
+        im1.save(mask_out)
+
+        f.close()
