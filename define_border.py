49336a397576f94dff66c77b6624ae88f9f17001
Luigi Masturzo <luigimasturzo@MacBook-Pro-di-Luigi.local>
2020-01-09 17:31:17 +0100

    improvements

--- define_border.py
+++ define_border.py
@@ -1,74 +1,33 @@
+import numpy as np
 from scipy.ndimage.filters import generic_filter
 from skimage.filters import threshold_otsu
-import numpy as np
-import math
-import pylab as plt
 from skimage.draw import line_nd
-'''distanaza euclidea'''
+
+'''Function that returns the euclidean distance.'''
 def distanza(x1,y1,x2,y2):
-    distanza_euclidea=np.sqrt((x1-x2)**2 + (y1-y2)**2)
+    distanza_euclidea = np.sqrt((x1-x2)**2 + (y1-y2)**2)
     return distanza_euclidea
 
-'''funsione che binarizza img'''
-def imbinarize(img):
-    thresh = threshold_otsu(img)
-    img[img >= thresh] = 1
-    img[img < thresh] = 0
-    return img
 
-def define_border_min(im_norm, NL, ROI,size_nhood_variance, Ray_masks):
-    roughborder=np.zeros(np.shape(im_norm))            #np.zeros(np.shape(im_norm))
-
-    p_x=[]
-    p_y=[]
-    d=[]
-
-    rr_arr=np.array([0])
-    cc_arr=np.array([0])
+'''
+The function creates a roughborder of the mass constituted by the points on the radial lines with maximum standard deviation.
+'''
+def define_border(img, NL, ROI,size_nhood_variance, Ray_masks):
+    
+    x_max=[]
+    y_max=[]
+    roughborder = np.zeros(np.shape(img))           
+    J = generic_filter(img, np.std, size=size_nhood_variance)
+    
     for _ in range (0, NL):
-        Jmasked=im_norm*Ray_masks[_]     #J*raggi=maschera dell'img
-        Jmasked=Jmasked*ROI
-        Jmasked[Jmasked==0]=10
-        w = np.where(Jmasked==np.min(Jmasked))
-        p_y.append(w[0][0])
-        p_x.append(w[1][0])
-        d.append(Jmasked[w[0][0],w[1][0]])
-
-    for _ in range(0,NL-1):
-        coords = line_nd((p_y[_],p_x[_]),(p_y[_+1],p_x[_+1]))
-        roughborder[coords[0],coords[1]]=1
-        rr_arr=np.hstack((rr_arr,coords[0]))
-        cc_arr=np.hstack((cc_arr,coords[1]))
-
-    return roughborder, rr_arr, cc_arr
-
-
-def define_border_max(im_norm, NL, ROI,size_nhood_variance, Ray_masks):
-    roughborder=np.zeros(np.shape(im_norm))            #np.zeros(np.shape(im_norm))
-    J=generic_filter(im_norm, np.std, size=size_nhood_variance)
-    '''
-    plt.figure('std')
-    plt.imshow(J)
-    plt.show()
-    '''
-    p_x=[]
-    p_y=[]
-    d=[]
-    rr_arr=np.array([])
-    cc_arr=np.array([])
-    for _ in range (0, NL):
-        Jmasked=J*Ray_masks[_]     #J*raggi=maschera dell'img
-        Jmasked=Jmasked*ROI
+        Jmasked = J*Ray_masks[_]
+        Jmasked = Jmasked*ROI
         w = np.where(Jmasked==np.max(Jmasked))
-        p_y.append(w[0][0])
-        p_x.append(w[1][0])
-        d.append(Jmasked[w[0][0],w[1][0]])
-
-
-    for _ in range(0,len(p_x)-1):
-        coords = line_nd((p_y[_],p_x[_]),(p_y[_+1],p_x[_+1]))
+        y_max.append(w[0][0])
+        x_max.append(w[1][0])
+        
+    for _ in range(0,len(x_max)-1):
+        coords = line_nd((y_max[_],x_max[_]),(y_max[_+1],x_max[_+1]))
         roughborder[coords[0],coords[1]]=1
-        rr_arr=np.hstack((rr_arr,coords[0]))
-        cc_arr=np.hstack((cc_arr,coords[1]))
-
-    return roughborder, rr_arr, cc_arr, p_x, p_y
+        
+    return roughborder
--- log_segm_program.py
+++ log_segm_program.py
@@ -4,121 +4,106 @@ import imageio
 import os
 import logging
 import glob
-import time
 from scipy.signal import convolve2d
 from skimage.transform import  rescale, resize
 from skimage import measure
 from skimage.filters import  median, threshold_yen , threshold_multiotsu
-from skimage import exposure
-from PIL import Image
 from scipy import ndimage
 from draw_radial_line import draw_radial_lines
-from define_border import define_border_min, distanza, define_border_max
+from define_border import define_border,distanza
 logging.basicConfig(level=logging.INFO)
 
 
-
+'''
+This function pre-process the image.
+'''
 def process_img(image, smooth_factor, scale_factor):
-    '''
-    C=np.min(image)/np.max(image)
-    #if C == 0:
-    p2, p98 = np.percentile(image, (2, 98))
-    image = exposure.rescale_intensity(image, in_range=(p2, p98))
-    '''
-    k = np.ones((smooth_factor,smooth_factor))/smooth_factor**2
-    im_conv=convolve2d(image, k )       #per ridurre il rumore (alte frequenze)
-    image_n = rescale(im_conv, 1/scale_factor)/np.max(im_conv)
-    
-    '''try logaritmic trasformation and averange filter'''
-    im_log=255*np.log10(image+1)
-    im_log=im_log.astype('uint8')
-    im_median= median(im_log, np.ones((5,5)))
-    im_res = resize(im_median, (126,126))
-    im_log_n = im_res/np.max(im_res)
-    
-    plt.figure()
-    plt.title('image')
-    plt.imshow(image_n)
-    plt.grid(True)
-    plt.colorbar()
-    plt.show()
 
-    return im_log_n, im_res, image_n
+    k = np.ones((smooth_factor,smooth_factor))/smooth_factor**2
+    im_conv = convolve2d(image, k )
+    image_normalized = rescale(im_conv, 1/scale_factor)/np.max(im_conv)
+    im_log = 255*np.log10(image+1)
+    im_log = im_log.astype('uint8')
+    im_median = median(im_log, np.ones((5,5)))
+    im_res = resize(im_median, (126,126))
+    im_log_normalized = im_res/np.max(im_res)
+
+    return im_log_normalized, image_normalized
 
 
 def find_center(x_max, y_max):
+    
     '''if the point with maximum intensity is too far away from the ROI center,
     the center is chosen as the center of the rectangle. This is also the starting point of rays.'''
+    
     if((np.abs(x_max-(x2-x1)/2)<(4/5)*(x1+(x2-x1)/2)) or (np.abs(y_max-(y2-y1)/2)<(4/5)*(y1+(y2-y1)/2))):
-        x_center=x1+int((x2-x1)/2)
-        y_center=y1+int((y2-y1)/2)
+        x_center = x1+int((x2-x1)/2)
+        y_center = y1+int((y2-y1)/2)
     else:
-        x_center=x_max[0]
-        y_center=y_max[0]
-    center=[x_center, y_center]
-    return (center)
+        x_center = x_max[0]
+        y_center = y_max[0]
+    center = [x_center, y_center]
+    return center
 
 if __name__ == '__main__':
     logging.info('Reading files')
-    #read all files
     logging.info('Enter images path.')
     logging.info('Luigi -> /Users/luigimasturzo/Documents/esercizi_fis_med/large_sample/*.png')
     logging.info('Sara -> /Users/sarasaponaro/Desktop/exam_cmpda/large_sample/*.png')
 
     file_path=str(input('file path = : '))
-
     fileID = glob.glob(file_path)
-
-
-    for _ in range (3,4):
-        f = open('center_list.txt', 'a')
-        image=imageio.imread(fileID[_])
-
-
-        filename, file_extension = os.path.splitext(fileID[_])
-        filename=os.path.basename(filename)
+    f = open('center_list.txt', 'a')
+    f.write('#filename\t x_center\t y_center\t y1\t x1\t y2\t x2\t')
+    
+    for index,item in enumerate(fileID):
+       
+        image=imageio.imread(item)
+        filename, file_extension = os.path.splitext(item)
+        filename = os.path.basename(filename)
         path_out = 'result/'
 
         if (os.path.exists(path_out)==False):
             logging.info ('Creating folder for save results.\n')
             os.makedirs('result')
 
-        file_out=path_out+filename+'_resized'+file_extension
-        mask_out=path_out+filename+'_mask'+file_extension
-
+        file_out = os.path.join(path_out,filename,'_resized',file_extension)
+        mask_out = os.path.join(path_out,filename,'_mask',file_extension)
+        
         logging.info('Defining parameters ')
-        smooth_factor= 8
-        scale_factor= 8
-        size_nhood_variance=5
-        NL=33
-        R_scale=5
+        smooth_factor = 8
+        scale_factor = 8
+        size_nhood_variance = 5
+        NL = 33
+        R_scale = 5
 
         logging.info('Processing image {}'.format(filename))
-             
-        im_log_n, im_resized , image_n= process_img(image, smooth_factor, scale_factor)
+        im_log_n, image_n= process_img(image, smooth_factor, scale_factor)
+        plt.figure()
+        plt.imshow(image_n)
+        plt.colorbar()
+        plt.grid()
+        plt.show()
+        
 
         logging.info('Starting the image segmentation.')
-
         decision  = 'si'
 
         while (decision != 'no'):
-
             logging.info('Enter ROIs coordinates that contains the mass.')
             y1=int(input('Enter y1: '))
             x1=int(input('Enter x1: '))
             y2=int(input('Enter y2: '))
             x2=int(input('Enter x2: '))
-
-
-            ROI=np.zeros(np.shape(image_n))                 #ones
-            ROI[y1:y2,x1:x2]=image_n[y1:y2,x1:x2]
-            y_max,x_max=np.where(ROI==np.max(ROI))
+            ROI = np.zeros(np.shape(image_n))                
+            ROI[y1:y2,x1:x2] = image_n[y1:y2,x1:x2]
+            y_max,x_max = np.where(ROI==np.max(ROI))
+            
             if (len(x_max) == 1):
                 center = find_center(x_max, y_max)
             else:
                 center = find_center(x_max[0], y_max[0])
-
-
+                
             logging.info('Showing ROI and center.' )
             plt.figure()
             plt.title('ROI')
@@ -127,108 +112,81 @@ if __name__ == '__main__':
             plt.plot(center[0], center[1], 'r.')
             plt.colorbar()
             plt.show()
-
             print('Do you want to change your coordinates?')
             decision=input('Answer yes or no: ')
 
-        f.write('{} \t {} \t {}\n'.format(filename, center[0], center[1]))
+        f.write('{} \t {} \t {}\t {}\t {}\t {}\t {}\n'.format(filename, center[0], center[1], y1, x1, y2, x2))
         
-        logging.info('Finding the border.')
-        
-        img=np.zeros(np.shape(image_n))
-        val=threshold_yen(image_n)
-        img[image_n >= val] = 1
-        img[image_n < val] = 0
-        
-        plt.figure('first step -> Yen threshold')
-        plt.imshow(img*ROI)
+        logging.info('First step: finding the border using Yen threshold.')
+        image_yen = np.zeros(np.shape(image_n))
+        val = threshold_yen(image_n)
+        image_yen[image_n >= val] = 1
+        image_yen[image_n < val] = 0
+        plt.figure()
+        plt.title('first step -> Yen threshold')
+        plt.imshow(image_yen*ROI)
         plt.plot(center[0], center[1], 'r.')
         plt.show()
         
-        logging.info('finding multiple otsu threshold')
-        p=np.zeros((y2-y1,x2-x1))
-        pp=np.zeros(np.shape(image_n))
-        p=img[y1:y2,x1:x2]*ROI[y1:y2,x1:x2]
-        val1, val2=threshold_multiotsu(p)
-        p[p>val2]=0
-        p[p<val1]=0
-        pp[y1:y2,x1:x2]=p
+        logging.info('Second step: refining border using Otsu double threshold')
+        image_otsu = np.zeros((y2-y1,x2-x1))
+        matrix = np.zeros(np.shape(image_n))
+        image_otsu = image_yen[y1:y2,x1:x2]*ROI[y1:y2,x1:x2]
+        val1, val2 = threshold_multiotsu(image_otsu)
+        image_otsu[image_otsu>val2] = 0
+        image_otsu[image_otsu<val1] = 0
+        matrix[y1:y2,x1:x2] = image_otsu
         
-        fill=np.zeros(np.shape(image_n))
-        contours = measure.find_contours(pp, 0)
-        arr=  contours[0]
-        arr = arr.flatten('F')
-        arr=arr.astype('int')
+        fill = np.zeros(np.shape(image_n))
+        contours = measure.find_contours(matrix, 0)
+        arr = contours[0].flatten('F').astype('int')
         y = arr[0:(int(len(arr)/2))]
         x = arr[(int(len(arr)/2)):]
-        fill[y,x]=1
-        fill=ndimage.binary_fill_holes(fill).astype(int)
-        
-        plt.figure('second step -> mask after double th.')
+        fill[y,x] = 1
+        fill = ndimage.binary_fill_holes(fill).astype(int)
+        plt.figure()
+        plt.title('second step -> Otsu double threshold')
         plt.imshow(fill)
+        plt.plot(center[0], center[1], 'r.')
         plt.show()
         
-        #define length of ray.
-        R=int(distanza(x1,y1,x2,y2)/2)
-        roi=np.zeros(np.shape(image_n))
-        roi[y1:y2,x1:x2]=1
-  
         logging.info('Refining segmentation results.')
-        roughborder_r=np.zeros(np.shape(im_log_n))
-        __px=[]
-        __py=[]
-        xx_r=[]
-        yy_r=[]
+        R=int(distanza(x1,y1,x2,y2)/2)
+        roughborder=np.zeros(np.shape(im_log_n))
+        
         for _ in range(0,len(x)):
             center_raff = [x[_], y[_]]
-            Ray_masks_raff = draw_radial_lines(img*ROI,center_raff,int(R/R_scale),NL)
-            roughborder_raff, _y , _x ,_px, _py= define_border_max(img*ROI, NL, fill ,size_nhood_variance, Ray_masks_raff)
-            roughborder_r+=roughborder_raff
-            xx_r=np.hstack((xx_r,_x))
-            yy_r=np.hstack((yy_r,_y))
-            __px+=_px
-            __py+=_py
-            
-        xx_r=xx_r.astype('int')
-        yy_r=yy_r.astype('int')
-            
+            Ray_masks_raff = draw_radial_lines(image_yen*ROI,center_raff,int(R/R_scale),NL)
+            roughborder_raff= define_border(image_yen*ROI, NL, fill ,size_nhood_variance, Ray_masks_raff)
+            roughborder+=roughborder_raff
         
-        fill_raff=ndimage.binary_fill_holes(roughborder_r).astype(int)
-        #fill_raff[__py,__px]=0
-        #fill_raff[yy_r,xx_r]=0
+        fill_raff=ndimage.binary_fill_holes(roughborder).astype(int)
         
-
         plt.figure()
         plt.title('Final mask of segmented mass.')
         plt.imshow(fill_raff)
         plt.show()
 
         logging.info('Showing result')
-        mass_only = fill_raff*image_n
-
         plt.figure()
         plt.title('Segmented mass.')
-        plt.imshow(mass_only)
+        plt.imshow(fill_raff*image_n)
         plt.colorbar()
         plt.show()
 
         plt.figure()
         plt.title('confronto.')
-        conf=imageio.imread('/Users/sarasaponaro/Desktop/exam_cmpda/large_sample_Im_segmented_ref/'+str(filename)+'_mass_mask.png')
+        conf=imageio.imread('/Users/luigimasturzo/Documents/esercizi_fis_med/large_sample_Im_segmented_ref/'+str(filename)+'_mass_mask.png')
         plt.imshow(conf)
         plt.imshow(image_n, alpha=0.3)
-        plt.imshow(mass_only, alpha=0.7)
+        plt.imshow(fill_raff, alpha=0.7)
         plt.show()
 
 
         '''
-        logging.info('Savening result.')
-        im = im_resized.astype(np.uint8)
-        im = Image.fromarray(im, mode='P')
-        im.save(file_out)
 
         fill_raff = fill_raff.astype(np.int8)
         im1 = Image.fromarray(fill, mode='L')
         im1.save(mask_out)
         '''
-        f.close()
+    f.close()
