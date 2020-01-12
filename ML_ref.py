8f0d83fda6967a1400f53642b8a0b75a45151101
SaraSaponaro <sarasaponaro@MBP-di-Sara-2.homenet.telecomitalia.it>
2020-01-12 15:46:13 +0100

    ML

--- ML_ref.py
+++ ML_ref.py
@@ -4,12 +4,12 @@ ref_machine_learning
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 import pylab as pl
 from pandas.plotting import scatter_matrix
 from matplotlib import cm
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
-
 
 
 mass = pd.read_table('feature_ref.txt', sep='\t',delim_whitespace=True,index_col=False, names=['filename','classe','area',' perimeter', 	 'circularity', 	' mu_NRL' ,	 'std_NRL', 	' zero_crossing', 	 'max_axis', 	' min_axis', 	'mu_VR', 	 'std_VR', 	 'RLE' ,	'convexity' ,	 'mu_I' 	' std_I', 'kurtosis', 	 'skewness'])
@@ -24,7 +24,7 @@ print(mass.groupby('classe').size())
 
 print('-----------------------------------')
 
-sns.countplot(fruits['classe'],label="Count")
+sns.countplot(mass['classe'],label="Count")
 plt.grid()
 plt.show()
 
