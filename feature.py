47d100193644f4ad6ebe774d0a7c9f745b387318
SaraSaponaro <sarasaponaro@host-131-114-213-119.m.unipi.it>
2020-01-13 17:04:25 +0100

    correction

--- feature.py
+++ feature.py
@@ -10,16 +10,12 @@ from scipy.stats import  kurtosis, skew
 from scipy.spatial import ConvexHull
 from log_segm_program import find_center
 
-
-
-
-
 def linear(x1,y1,x2,y2):
     """
     Find angular coefficient and intercept of a straight line given two points
     """
     m = (y2-y1)/(x2-x1)
-    q = y1 - x1*(y2-y1)/(x2-x1)
+    q = (y1 - x1*(y2-y1))/(x2-x1)
     return m,q
 
 def mass_area(mask_only):
@@ -34,7 +30,7 @@ def mass_perimeter(mask_only):
     """
     Summing up the number of pixels on the boundary's mass
     """
-    contours = measure.find_contours(mask_only, 0)
+    contours = measure.find_contours(mask_only, 0, fully_connected='high')
     return len(contours[0])
 
 def circularity(area, perimetro):
@@ -46,7 +42,7 @@ def NRL(mask_only, center_x, center_y, perimetro):
     Finds the border of the mask and computes the distance between the boundary pixels and the center.
     It also calculates the normalized distance, the mean and the standard deviation of the normalized distance.
     """
-    contours = measure.find_contours(mask_only, 0)
+    contours = measure.find_contours(mask_only, 0, fully_connected='high')
     arr = contours[0]
     arr = arr.flatten('F')
     y = arr[0:(int(len(arr)/2))]
@@ -54,7 +50,7 @@ def NRL(mask_only, center_x, center_y, perimetro):
     d = distanza(center_x,center_y, x, y)
     d_norm = d/(np.max(d))
     d_mean = np.sum(d_norm)/perimetro
-    s = np.sum((d-d_mean)**2)
+    s = np.sum((d_norm-d_mean)**2)
     std = np.sqrt(s/perimetro)
     return d, d_mean, d_norm, std
 
@@ -83,10 +79,10 @@ def axis(mask_only, center_x, center_y):
     """
     Finds minimum and maximum distance connecting two boundary pixels passing trough the center.
     """
-    contours = measure.find_contours(mask_only, 0)
+    contours = measure.find_contours(mask_only, 0, fully_connected='high')
     arr = contours[0].flatten('F')
-    y = arr[0:1+int(len(arr)/2)]
-    x = arr[1+int(len(arr)/2):]
+    y = arr[0:int(len(arr)/2)]
+    x = arr[int(len(arr)/2):]
     l_list = []
 
     for itemx, itemy in zip(x, y):
@@ -138,68 +134,55 @@ def mass_intensity(mass):
 
 if __name__ == '__main__':
     logging.info('Reading files.')
-    #files = glob.glob('/Users/sarasaponaro/Desktop/exam_cmpda/large_sample_Im_segmented_ref/*_resized.png')
-    #masks = glob.glob('/Users/sarasaponaro/Desktop/exam_cmpda/large_sample_Im_segmented_ref/*_mask.png')
-    files = glob.glob('/Users/luigimasturzo/Documents/esercizi_fis_med/large_sample_Im_segmented_ref/*_resized.png')
-    masks = glob.glob('/Users/luigimasturzo/Documents/esercizi_fis_med/large_sample_Im_segmented_ref/*_mask.png')
+    files = glob.glob('/Users/sarasaponaro/Desktop/exam_cmpda/large_sample_Im_segmented_ref/*_resized.png')
+    masks = glob.glob('/Users/sarasaponaro/Desktop/exam_cmpda/large_sample_Im_segmented_ref/*_mask.png')
+    #files = glob.glob('/Users/luigimasturzo/Documents/esercizi_fis_med/large_sample_Im_segmented_ref/*_resized.png')
+    #masks = glob.glob('/Users/luigimasturzo/Documents/esercizi_fis_med/large_sample_Im_segmented_ref/*_mask.png')
     files.sort()
     masks.sort()
     nametxt = 'feature_ref.txt'
+    nametxt_m = 'feature_m.txt'
+    nametxt_b= 'feature_b.txt'
     f = open(nametxt, 'w')
-    f.write('filename \t classe \t area \t perimeter \t circularity \t mu_NRL \t std_NRL \t zero_crossing \t max_axis \t min_axis \t')
-    f.write('mu_VR \t std_VR \t RLE \t convexity \t mu_I \t std_I \t kurtosis \t skewness\n')
+    fm = open(nametxt_m, 'w')
+    fb = open(nametxt_b, 'w')
+    #f.write('filename \t classe \t area \t perimeter \t circularity \t mu_NRL \t std_NRL \t zero_crossing \t max_axis \t min_axis \t')
+    #f.write('mu_VR \t std_VR \t RLE \t convexity \t mu_I \t std_I \t kurtosis \t skewness\n')
     for index, item in enumerate(masks):
         filename, file_extension = os.path.splitext(item)
         filename = os.path.basename(filename)
-        filename = filename[:-10]           
+        filename = filename[:-10]
         "1: malignant 2:benign"
-        classe = filename[-1]         
+        classe = filename[-1]
+
         mask_only = imageio.imread(item)
         img = imageio.imread(files[index])
         mass = img*mask_only
-        
-        """
-        provo nuovo metodo per trovare il centro
-        """
-        
+
         a=np.where(mass!=0)
         center_intensity=np.where(mass==np.max(mass))
-        
+
         x1=np.min(a[1])
         y1=np.min(a[0])
         x2=np.max(a[1])
         y2=np.max(a[0])
-        xmax=center_intensity[1]
-        ymax=center_intensity[0]
         
-        print('------------------------',index,'-----------------------------')
-        print('len(xmax) = ', len(xmax))
-        
-        if (len(xmax) == 1):
-            print('l = 1')
-            center = find_center(xmax, ymax, y1, x1, y2, x2)
+        if (len(center_intensity[1]) == 1):
+            center = find_center(center_intensity[1], center_intensity[0], y1, x1, y2, x2)
         else:
-            print('l = boh')
-            print('xmax',xmax)
-            print('ymax',ymax)
-            print(y1, x1, y2, x2)
-            center = find_center(xmax[0], ymax[0], y1, x1, y2, x2)
-            
-        
-        
+            center = find_center(center_intensity[1][0],center_intensity[0][0], y1, x1, y2, x2)
 
-        
-        #center = find_center(xmax, ymax, y1, x1, y2, x2)
         center_x=center[0]
         center_y=center[1]
-        
-    
+
+
         area = mass_area(mask_only)
         p = mass_perimeter(mask_only)
         circ = circularity(area,p)
         d, mu_NRL, d_norm, std_NRL = NRL(mask_only, center_x, center_y, p)
         cross0 = cross_zero(d, mu_NRL)
-        rmax, rmin = axis(mask_only, center_x, center_y)
+        rmin,rmax  = axis(mask_only, center_x, center_y)
+        print(index,'----------- min e max',rmin,rmax)
         #print(rmax)
         vm, vs = var_ratio(d, mu_NRL)
         E = Radial_lenght_entropy(d_norm)
@@ -208,6 +191,16 @@ if __name__ == '__main__':
         intensity = np.reshape(mass, -1)
         kurt = kurtosis(intensity)
         sk = skew(intensity)
+        if classe == '1':
+
+            fm.write('{} \t{} \t{} \t{} \t{} \t{} \t{} \t{} \t '.format(filename, classe, area, p, circ ,mu_NRL, std_NRL, cross0))
+            fm.write('{} \t{} \t{} \t{} \t{} \t{} \t{} \t{} \t{} \t{}   \n'.format(rmax, rmin, vm, vs, E, conv, im, istd, kurt, sk))
+        elif classe == '2':
+
+            fb.write('{} \t{} \t{} \t{} \t{} \t{} \t{} \t{} \t '.format(filename, classe, area, p, circ ,mu_NRL, std_NRL, cross0))
+            fb.write('{} \t{} \t{} \t{} \t{} \t{} \t{} \t{} \t{} \t{}   \n'.format(rmax, rmin, vm, vs, E, conv, im, istd, kurt, sk))
         f.write('{} \t{} \t{} \t{} \t{} \t{} \t{} \t{} \t '.format(filename, classe, area, p, circ ,mu_NRL, std_NRL, cross0))
         f.write('{} \t{} \t{} \t{} \t{} \t{} \t{} \t{} \t{} \t{}   \n'.format(rmax, rmin, vm, vs, E, conv, im, istd, kurt, sk))
+    fm.close()
+    fb.close()
     f.close()
