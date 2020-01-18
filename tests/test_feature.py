45e17eca559f807aeb1f15f6a105fcb81d5217b3
SaraSaponaro <sarasaponaro@MBP-di-Sara-2.homenet.telecomitalia.it>
2020-01-18 14:32:23 +0100

    tests

--- tests/test_feature.py
+++ tests/test_feature.py
@@ -6,17 +6,19 @@ import pylab as plt
 import unittest
 from skimage.draw import circle_perimeter
 from scipy import ndimage
-
+from feature import mass_area
 
 
 
 test=np.zeros((126,126),dtype=np.uint8)
 rr, cc = circle_perimeter(63, 63, 10)
 test[rr,cc]=1
-fill=ndimage.binary_fill_holes(img).astype(int)
+circle=ndimage.binary_fill_holes(test).astype(int)
 
-class Test_draw_radial_lines(unittest.TestCase):
-    def test_feature(self):
+
+class Test_feature(unittest.TestCase):
+    def mass_area(self):
+        self.assertAlmostEqual(mass_area(circle),np.pi*100)
         
         
 
