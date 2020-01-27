a041b20b8d02b40ca0c1aceb966b923e0666aebd
SaraSaponaro <55702505+SaraSaponaro@users.noreply.github.com>
2020-01-27 16:40:12 +0100

    Update __init__.py

---
+++ segmentation_program/__init__.py
@@ -0,0 +1,17 @@
+import os
+import logging
+
+
+# Basic local environment.
+#
+BASE_FOLDER = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
+
+
+def _join(*args) -> str:
+    """Path concatenation relatove to the bas package folder (avoids some typing.)
+    """
+    return os.path.join(BASE_FOLDER, *args)
+
+
+ROOT_FOLDER = _join('segmantation_program')
+TEST_FOLDER = _join('tests')
