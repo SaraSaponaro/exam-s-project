17b4c182fcafd80b30f7cc86c1998c4020b6143d
SaraSaponaro <sarasaponaro@host-131-114-215-48.m.unipi.it>
2020-01-28 11:38:22 +0100

    setup

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# See this stackoverflow question
+# http://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
+# for the magic in this command
+SETUP_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+#
+# Add the root folder to the $PYTHONPATH so that we can effectively import
+# the relevant modules.
+#
+export PYTHONPATH=$SETUP_DIR:$PYTHONPATH
+echo "PYTHONPATH set to " $PYTHONPATH
